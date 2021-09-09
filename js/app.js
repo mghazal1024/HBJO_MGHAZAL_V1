0fa76456276a04fb4bcdad1a4a34c999cca96896
mghazal1024 <mghazal1024@gmail.com>
2021-09-09 17:53:52 +0200

    added carousel function to marques

--- js/app.js
+++ js/app.js
@@ -6,3 +6,18 @@ navButton.addEventListener('click', () => {
     navButton.classList.toggle('opened');
     mobileNav.classList.toggle('opened');
 })
+
+
+// NOS MARQUES
+
+let elem = document.querySelector('.main-carousel');
+
+let flkty = new Flickity( '.main-carousel', {
+  cellAlign: 'left',
+  contain: true,
+  groupCells: false,
+  wrapAround: true,
+  autoPlay: 2000
+});
+
+
